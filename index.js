1be5c1b2212add8e4a0e3a790548e14b0282cc93
Jure Stepisnik <jure.stepisnik@tretton37.com>
2021-02-11 17:08:20 +0100

    Added animation and quote cards

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-( () => {
-  const dontClickButton = document.querySelector('#dontClick');
-  const byeSection = document.querySelector('.bye');
-  const wave = document.querySelector('.wave');
-  dontClickButton.addEventListener('click', () => {
-    byeSection.style.display = 'none';
-    wave.style.display = '';
+(() => {
+  const dontClickButton = document.querySelector("#dontClick");
+  const byeSection = document.querySelector(".bye");
+  const wave = document.querySelector(".wave");
+  const quote = document.querySelector(".quote");
+  dontClickButton.addEventListener("click", () => {
+    byeSection.style.display = "none";
+    wave.style.display = "";
+    quote.style.display = "block";
+
+    setTimeout(() => {
+      const arr = [
+        { message: "All" },
+        { message: "you" },
+        { message: "need" },
+        { message: 3 },
+        { message: {} },
+        { message: "is" },
+        { message: "map," },
+        { message: "filter," },
+        { message: "and" },
+        { message: "reduce" },
+      ];
+
+      const saying = arr
+        .map((elem) => elem.message)
+        .filter((word) => typeof word === "string")
+        .reduce((accumulator, elem) => (accumulator += ` ${elem}`), "");
+
+      console.log(`"${saying}" -- Klemen`);
+    }, 3000);
   });
 })();
